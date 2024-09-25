c0fd992fdec42815211a3e0f4f7d9bf31c4056c2
lipolipinski <lipolipinski@gmail.com>
2024-09-25 08:01:08 +0200

    Initial Commit

---
+++ hello-world/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  hello-world
+//
+//  Created by Adrian Lipinski on 21/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var serchtext = ""
+    @State var confirm = "some text shold be addeded above"
+    var body: some View {
+        VStack(alignment: .center) {
+            //            Image(systemName: "car")
+            //                .imageScale(.small)
+            //                .foregroundStyle(.blue)
+            Spacer()
+            TextField("Try here: ", text: $serchtext)
+                .font(Font.system(size: 30))
+                .foregroundColor(.black)
+                .padding(30)
+                .background(RoundedRectangle(cornerRadius: 25.0).fill(Color.red))
+            Spacer()
+            Text(confirm)
+            Spacer()
+            Button(action: submit){
+                Text("Awesome Button")}
+//            allowsHitTesting(true)
+            Spacer()
+        }
+        .padding()
+    }
+    
+    
+    func submit() {
+        confirm = "😃"
+        
+    }}
+
+#Preview {
+    ContentView()
+}
---
+++ hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Adrian Lipinski on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
